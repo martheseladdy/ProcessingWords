920745dcf64598a0786402fc0e34f3f938123256
Marthese Ladvier <48213746+martheseladdy@users.noreply.github.com>
2022-07-22 12:20:22 +0100

    Test methods written, not implemented, for UserInput

--- src/main/java/UserInput.java
+++ src/main/java/UserInput.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class userInput {
+public class UserInput {
+
+    //take text file
+    //assume .txt
+    //assume in same directory
+    //pass text file to api
+    //recieve data from api
+    //display data
+
+    public void main (String[] args){
+        System.out.println("Hi there! Please enter the filename of the text file (.txt) that you wish to be processed.");
+    }
+    public String sanitiseInput(String userInput){
+        return null;
+    }
+    public String readFile(String filename){
+        return null;
+    }
+
+    public void displayResults(String results){
+    }
 }
--- src/test/java/UserInputTest.java
+++ src/test/java/UserInputTest.java
@@ -1,4 +1,46 @@
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserInputTest {
-  
+
+    @org.junit.jupiter.api.Test
+    void GivenUserInputForFilename_WhenFilenameIncludesFileType_ThenRemoveFileType() {
+        fail("Not Implemented Yet");
+    }
+    @org.junit.jupiter.api.Test
+    void GivenUserInputForFilename_WhenFilenameIncludesFormatIssues_ThenCastToStringType() {
+        fail("Not Implemented Yet");
+    }
+
+    @org.junit.jupiter.api.Test
+    void GivenUserInputForFilename_WhenFilenameDoesNotExist_ThenThrowError() {
+        fail("Not Implemented Yet");
+    }
+
+    @org.junit.jupiter.api.Test
+    void GivenSanitisedFilename_WhenFileHasExpectedContent_ThenReadFileContent() {
+        fail("Not Implemented Yet");
+    }
+    @org.junit.jupiter.api.Test
+    void GivenSanitisedFilename_WhenFileIsEmpty_ThenThrowError() {
+        fail("Not Implemented Yet");
+    }
+
+    @org.junit.jupiter.api.Test
+    void GivenSanitisedFilename_WhenFileIsVeryLong_ThenReadFileContent() {
+        fail("Not Implemented Yet");
+    }
+
+    @org.junit.jupiter.api.Test
+    void GivenSanitisedFilename_WhenFileIsOneWord_ThenReadFileContent() {
+        fail("Not Implemented Yet");
+    }
+
+    @org.junit.jupiter.api.Test
+    void GivenProcessingResults_WhenExpectedDataTypes_ThenDisplayResults() {
+        fail("Not Implemented Yet");
+    }
+    @org.junit.jupiter.api.Test
+    void GivenProcessingResults_WhenNullOrUnexpected_ThenThrowError() {
+        fail("Not Implemented Yet");
+    }
 }
